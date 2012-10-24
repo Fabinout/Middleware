70f88480a56a3805b510496b62eb38ff58b85441
Lionel Becuwe <lionel.becuwe@gmail.com>
2012-10-24 20:37:56 +0200

    Directory motified for filereader Exercice.java file moved out src folder.

--- MiddleWare2.0/src/web/TestClient.java
+++ MiddleWare2.0/src/web/TestClient.java
@@ -2,7 +2,6 @@ package web;
 
 import java.io.File;
 import java.io.FileReader;
-import java.nio.CharBuffer;
 
 import model.SourceFile;
 
@@ -13,21 +12,20 @@ import Compilation.Main;
 
 public class TestClient {
 
-    /**
-     * @param args
-     * @throws ResourceException
-     */
-    public static void main(String[] args) throws Exception {
+	/**
+	 * @param args
+	 * @throws ResourceException
+	 */
+	public static void main(String[] args) throws Exception {
 
-        ClientResource cr = new ClientResource("http://localhost:8182/source");
+		ClientResource cr = new ClientResource("http://localhost:8182/source");
 
-        // Add a source file
-        SourceFile s = new SourceFile();
-        s.setName("exemple");
-        FileReader rs = new FileReader(new File(Main.dir, "/src/Compilation/Exercice.java)"));
-        char[] bs = new char[8];
-        
-       
-    }
+		// Add a source file
+		SourceFile s = new SourceFile();
+		s.setName("exemple");
+		FileReader rs = new FileReader(new File(Main.dir,"Exercice.java)"));
+		char[] bs = new char[8];
+
+	}
 
 }
